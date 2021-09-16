effc07d0740cf991e04cb7162273ff425bc83219
Helena <nenafgreco@hotmail.com>
2021-09-16 17:12:13 -0300

    Requisito 2 e consertando o not found na tela

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
-import Login from './components/Login';
-import Search from './components/Search';
-import Album from './components/Album';
-import Favorites from './components/Favorites';
-import Profile from './components/Profile';
-import ProfileEdit from './components/ProfileEdit';
-import NotFound from './components/NotFound';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Login from './pages/Login';
+import Search from './pages/Search';
+import Album from './pages/Album';
+import Favorites from './pages/Favorites';
+import Profile from './pages/Profile';
+import ProfileEdit from './pages/ProfileEdit';
+import NotFound from './pages/NotFound';
 
 class App extends React.Component {
   render() {
     return (
       <BrowserRouter>
         <p>TrybeTunes in progress</p>
-        <Route path="/" component={ Login } />
-        <Route path="/search" component={ Search } />
-        <Route path="/album/:id" component={ Album } />
-        <Route path="/favorites" component={ Favorites } />
-        <Route path="/profile" component={ Profile } />
-        <Route path="/profile/edit" component={ ProfileEdit } />
-        <Route path="*" component={ NotFound } />
+        <Switch>
+          <Route path="/" component={ Login } />
+          <Route path="/search" component={ Search } />
+          <Route path="/album/:id" component={ Album } />
+          <Route path="/favorites" component={ Favorites } />
+          <Route path="/profile" component={ Profile } />
+          <Route path="/profile/edit" component={ ProfileEdit } />
+          <Route path="*" component={ NotFound } />
+        </Switch>
       </BrowserRouter>
     );
   }
---
+++ src/pages/Login.jsx
@@ -0,0 +1,65 @@
+import React from 'react';
+import { Redirect } from 'react-router-dom';
+import { createUser } from '../services/userAPI';
+import Loading from '../component/Loading';
+
+/** Ref: Consultei o repositório do Glauco sobre a função Logged */
+class Login extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      inputName: '',
+      loading: false,
+      logged: false,
+    };
+  }
+
+  handleChange = (event) => {
+    this.setState({ inputName: event.target.value });
+  }
+
+  Logged = () => {
+    const { inputName } = this.state;
+    this.setState({ loading: true });
+    createUser({ name: inputName })
+      .then(() => this.setState({
+        loading: false,
+        logged: true,
+      }));
+  }
+
+  render() {
+    const minNameLength = 3;
+    const { inputName, logged, loading } = this.state;
+    if (logged) {
+      return <Redirect to="/search" />;
+    }
+    return (
+      <div data-testid="page-login">
+        {loading ? (<Loading />)
+          : (
+            <>
+              <label htmlFor="login-name-input">
+                Nome Completo:
+                <input
+                  type="text"
+                  data-testid="login-name-input"
+                  onChange={ this.handleChange }
+                />
+              </label>
+              <button
+                type="submit"
+                data-testid="login-submit-button"
+                disabled={ inputName.length < minNameLength }
+                onClick={ this.Logged }
+              >
+                Entrar
+              </button>
+            </>
+          )}
+      </div>
+    );
+  }
+}
+
+export default Login;
