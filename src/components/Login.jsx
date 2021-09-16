ebb827840c88fb923a10efab648a7fb129e2aff7
Helena <nenafgreco@hotmail.com>
2021-09-15 22:43:17 -0300

    Requisito 2 - parte I

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,10 +1,27 @@
 import React from 'react';
 
 class Login extends React.Component {
+  // construct() {
+  //   super()
+  //   this.state = {
+  //   }
+  // }
   render() {
     return (
       <div data-testid="page-login">
-        <p>Login</p>
+        <label htmlFor="login-name-input">
+          Nome Completo:
+          <input
+            type="text"
+            data-testid="login-name-input"
+          />
+        </label>
+        <button
+          type="submit"
+          data-testid="login-submit-button"
+        >
+          Entrar
+        </button>
       </div>
     );
   }
